dfa20082d4555c0c55ba3c8bb3d9a3abef2f0ed0
Francis Tseng <ftzeng@gmail.com>
2013-05-01 10:19:11 -0400

    add some transitions

--- scripts/app.js
+++ scripts/app.js
@@ -32,15 +32,19 @@ function( $, ø, magic, hbs, tpl, data, empty_tpl ) {
 				return false;
 			});
 
-			// Back & initial load
+			s.load( s.data );
+
+
+			// Back (popstate)
+			// Check for initial (useless) popstate.
+			// http://goo.gl/pA8cN
+			var popped = ('state' in window.history && window.history.state !== null), initialURL = location.href;
 			$(window).bind("popstate", function() {
-				if ( window.location.hash ) {
-					var targets = window.location.hash.substring(1).split("/");
-					var results = s.traverse( targets, s.data );
-					s.render( results );
-				} else {
-					s.render( s.data );
-				}
+				var initialPop = !popped && location.href == initialURL
+				popped = true
+				if ( initialPop ) return
+
+				s.load( s.data );
 			});
 		},
 
@@ -56,36 +60,57 @@ function( $, ø, magic, hbs, tpl, data, empty_tpl ) {
 			return data[target];
 		},
 
-		render: function( data ) {
-			$(".library").empty();
-
-			if ( ø.isEmptyObj(data) ) {
-				var content = this.empty_template();
-				console.log(content);
-				$(".library").append( content );
-
+		load: function( data ) {
+			var s = this;
+			if ( window.location.hash ) {
+				var targets = window.location.hash.substring(1).split("/");
+				var results = s.traverse( targets, data );
+				s.render( results );
 			} else {
-				for (var key in data) {
-					var url = "";
-					if (window.location.hash === "") {
-						url += "#";
-					} else {
-						url += window.location.hash + "/";
-					}
+				s.render( data );
+			}
+		},
 
-					if ( this.data[key] instanceof Array ) {
-						var books = data[key];
-						for (var i = 0; i < books.length; i++) {
-							var file = books[i]
-								, item = this.template({ title: file, url: url.substring(1) + file });
+		render: function( data ) {
+			var s = this;
+			$(".library").animate({
+				opacity: 0,
+				marginTop: "-6em"
+			}, function() {
+				$(".library").empty();
+
+				if ( ø.isEmptyObj(data) ) {
+					var content = s.empty_template();
+					$(".library").append( content );
+
+				} else {
+					for (var key in data) {
+						var url = "";
+						if (window.location.hash === "") {
+							url += "#";
+						} else {
+							url += window.location.hash + "/";
+						}
+
+						if ( data[key] instanceof Array ) {
+							var books = data[key];
+							for (var i = 0; i < books.length; i++) {
+								var file = books[i]
+									, item = s.template({ title: file, url: url.substring(1) + file });
+								$(".library").append( item );
+							}
+						} else {
+							var item = s.template({ title: key, url: url + key, collection: true });
 							$(".library").append( item );
 						}
-					} else {
-						var item = this.template({ title: key, url: url + key, collection: true });
-						$(".library").append( item );
 					}
 				}
-			}
+
+				$(".library").animate({
+					opacity: 1,
+					marginTop: "0em"
+				});
+			});
 		}
 	}
 });
